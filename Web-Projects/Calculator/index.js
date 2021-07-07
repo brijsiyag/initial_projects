919ecd55d103b134c32876e93aba9f17a79bc339
brijsiyag <brijsiyag@gmail.com>
2021-07-07 18:28:35 +0530

    button-beep

--- Web-Projects/Calculator/index.js
+++ Web-Projects/Calculator/index.js
@@ -1,51 +1,56 @@
 
+var audio = new Audio("Audio/beep.mov");
+
 var main = "";
 document.querySelector(".display").innerText = main;
-// for(var i = 0;i<10;i++){
-//     var x = ".element-"+i;
-//     document.querySelector(x).addEventListener("click",function () {
-//         main += document.querySelector(x).innerText;
-//         document.querySelector(".display").innerText = main;
-//     });
-// }
 
 document.querySelector(".element-1").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-1").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-2").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-2").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-3").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-3").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-4").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-4").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-5").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-5").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-6").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-6").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-7").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-7").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-8").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-8").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-9").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-9").innerText;
     document.querySelector(".display").innerText = main;
 });
 document.querySelector(".element-0").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-0").innerText;
     document.querySelector(".display").innerText = main;
 });
@@ -57,6 +62,7 @@ document.querySelector(".element-0").addEventListener("click",function () {
 
 
 document.querySelector(".element-plus").addEventListener("click",function () {
+    audio.play();
     if(main[main.length-1] != "+" && main[main.length-1] != "-" && main[main.length-1] != "*" && main[main.length-1] != "/")
     main += "+";
     else{
@@ -68,6 +74,7 @@ document.querySelector(".element-plus").addEventListener("click",function () {
 
 
 document.querySelector(".element-minus").addEventListener("click",function () {
+    audio.play();
     if(main[main.length-1] != "+" && main[main.length-1] != "-" && main[main.length-1] != "*" && main[main.length-1] != "/")
     main += "-";
     else{
@@ -79,6 +86,7 @@ document.querySelector(".element-minus").addEventListener("click",function () {
 
 
 document.querySelector(".element-multiply").addEventListener("click",function () {
+    audio.play();
     if(main[main.length-1] != "+" && main[main.length-1] != "-" && main[main.length-1] != "*" && main[main.length-1] != "/")
     main += "*";
     else{
@@ -90,6 +98,7 @@ document.querySelector(".element-multiply").addEventListener("click",function ()
 
 
 document.querySelector(".element-divide").addEventListener("click",function () {
+    audio.play();
     if(main[main.length-1] != "+" && main[main.length-1] != "-" && main[main.length-1] != "*" && main[main.length-1] != "/")
     main += "/";
     else{
@@ -104,6 +113,7 @@ document.querySelector(".element-divide").addEventListener("click",function () {
 
 
 document.querySelector(".element-plus-minus").addEventListener("click",function () {
+    audio.play();
     if(main[0] != "-")
     {
         main = "-" + main;
@@ -116,6 +126,7 @@ document.querySelector(".element-plus-minus").addEventListener("click",function 
 
 
 document.querySelector(".element-open").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-open").innerText;
     document.querySelector(".display").innerText = main;
 });
@@ -123,23 +134,27 @@ document.querySelector(".element-open").addEventListener("click",function () {
 
 
 document.querySelector(".element-close").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-close").innerText;
     document.querySelector(".display").innerText = main;
 });
 
 
 document.querySelector(".element-clear").addEventListener("click",function () {
+    audio.play();
     main = main.slice(0,main.length-1);
     document.querySelector(".display").innerText = main;
 });
 
 document.querySelector(".element-clear").addEventListener("dblclick",function () {
+    audio.play();
     main = "";
     document.querySelector(".display").innerText = main;
 });
 
 
 document.querySelector(".element-dot").addEventListener("click",function () {
+    audio.play();
     main += document.querySelector(".element-dot").innerText;
     document.querySelector(".display").innerText = main;
 });
@@ -148,6 +163,7 @@ document.querySelector(".element-equal").addEventListener("click",equal);
 
 
 function equal() {
+    audio.play();
     main = eval(main);
     main += "";
     document.querySelector(".display").innerText = main;
